6243f03aaea0266ead32688380c98beec5c372cc
Princeray-18 <rayp11348@gmail.com>
2025-08-05 11:47:51 +0530

    learn about js

---
+++ DAY13.js
@@ -0,0 +1,44 @@
+//  let arr = (5, "chandan", true)
+// console.log(arr)
+ 
+// //     function declartion
+
+// function sayhello(){
+//     console.log("hello");
+// }
+
+// //  function call
+// // sayhello() 
+ 
+// // // Parameterized function 
+// function chandan(person,person1){
+//      console.log(person+" handsome",+person1)
+//  }
+
+//  function chandan(person, person1) {
+//     console.log(person + " handsome " + person1);
+// }
+//  chandan("chandan")
+//     chandan("akshat")
+//     chandan("rudra")
+//     chandan("prince")
+// function func(person,person1){
+// console.log(person-(+person1))
+// }
+// function (x "2")
+
+// function add(a,b){
+//     return a+b
+// }
+// let result= add(4,5);
+// console.log(result)
+
+// Example function declaration
+// function printValue(x) {
+//     console.log(x);
+// }
+
+// // function add(a, b) {
+// //     return a + b;
+// // }
+// // let result = add(4, 5);
