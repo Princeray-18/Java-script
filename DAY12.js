c8ed85c049e549c084f3aeaf5476181bd919955d
Princeray-18 <rayp11348@gmail.com>
2025-08-05 17:15:20 +0530

    learn something new

--- DAY12.js
+++ DAY12.js
@@ -1,15 +1,15 @@
 
-// let pr = new Promise((resolve, reject) => {
-//     let success = false; 
-//     if ( !success) {
-//         resolve("Kam Tera Hogaya");
-//     } else {
-//         reject("Kam Tera Nahi Hogaya");
-//     }
-// });
-// pr.then((Prince) => {
-//     console.log(Prince);
-// }).catch((sipun) => {
-//     console.log(sipun);
-// });
+let pr = new Promise((resolve, reject) => {
+    let success = true; 
+    if ( ! success) {
+        resolve("Kam Tera Hogaya");
+    } else {
+        reject("Kam Tera Nahi Hua");
+    }
+});
+pr.then((Prince) => {
+    console.log(Prince);
+}).catch((sipun) => {
+    console.log(sipun);
+});
 
