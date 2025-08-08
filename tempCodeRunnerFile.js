4428c342fa48c00adc1a961788361fc6aedf4498
Princeray-18 <rayp11348@gmail.com>
2025-08-09 00:07:12 +0530

    changes

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+let person = { name: "Prince", age: 22 };  
+console.log(person);
+let fruits = ["apple", "banana", "mango"]; 
+console.log(fruits);
+
+function greet() {
+  console.log("Hello Prince");
+}
+
+greet();
