d5f6c7ff9d800b7436c4b3c38ce3f1912f109105
Princeray-18 <rayp11348@gmail.com>
2025-08-14 02:00:50 +0530

    new

--- DAY9.js
+++ DAY9.js
@@ -24,12 +24,12 @@
 // console.log (Object.entries(student));
 
 // object Destructuring
- let bag={
-    daba1: "chicken",
- daba2:"egg",
- }
- let {daba1:contier1, daba2:cointer2} = bag;
- console.log(contier1, cointer2);
+//  let bag={
+//     daba1: "chicken",
+//  daba2:"egg",
+//  }
+//  let {daba1:contier1, daba2:cointer2} = bag;
+//  console.log(contier1, cointer2);
 // let bag ={
 //     item1:"coffee",
 //     item2:"Tea,"
